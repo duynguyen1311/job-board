6ac2fce01bc05339151b0b91624d3b2fa3d42e65
duynguyen <duy0256@gmail.com>
2024-09-29 17:40:01 +0700

    Load job data and company data from database

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,14 +1,19 @@
-import {getJobs} from "./db/jobs.js";
+import {getJob, getJobs, getJobsByCompany} from "./db/jobs.js";
 import {getCompany} from "./db/companies.js";
 
 export const resolvers = {
     Query: {
+        job: (_root, {id}) => getJob(id),
+        company: (_root, {id}) => getCompany(id),
         jobs: () => getJobs()
     },
     Job: {
         company: (job) => getCompany(job.companyId),
         date: (job) => toIsoDate(job.createdAt)
     },
+    Company: {
+        jobs : (company) => getJobsByCompany(company.id)
+    }
 };
 
 function toIsoDate(value){
